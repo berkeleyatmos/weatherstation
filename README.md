56f7017bd6d9958293099c391669d88f183b7135
Henry Peterson <hankpete97@gmail.com>
2018-10-22 18:01:18 -0700

    new organization

--- README.md
+++ README.md
@@ -7,12 +7,12 @@ The following README will serve as an introductory guide on how to use the stati
 
 # Setup
 
-On Airbears2 the station's IP address is 10.142.93.19. 
+On Airbears2 the station's IP address is 10.142.21.24. 
 Add the following line to your `~/.ssh/config` so that you can SSH to the Pi with just `ssh weatherstation`:
 
 ```bash
 Host weatherstation
-	HostName 10.142.93.19
+	HostName 10.142.21.24
 	User pi
 ```
 
--- call_sensor_loop.py
+++ call_sensor_loop.py
@@ -3,6 +3,7 @@
 import pigpio
 import DHT22
 import os
+import csv
 from time import sleep
 
 # instantiate a pi object
@@ -15,11 +16,20 @@ s = DHT22.sensor(pi, 4)
 s.trigger()
 
 # trigger the sensor every few seconds
-dt = 5 
-while True:
-    sleep(1)
-    s.trigger()
-    hum = s.humidity()
-    temp = s.temperature()
-    print("RH: {:3.1f} % T: {:3.1f} C".format(hum, temp))
-    
+dt = 1 #note: when running data, dt will likely be 300 or something higher.
+
+# temporary counter. Fix the while loop so it runs automatically in the future.
+counter = 15
+
+# open the output file
+with open('humidity_temperature.csv', 'w') as data_file:
+    data = csv.writer(data_file, delimiter = '\n')
+
+    while counter > 0:
+        sleep(dt)
+        s.trigger()
+        hum = s.humidity()
+        temp = s.temperature()
+        print(str(counter) + ' seconds remaining in the program.')
+        counter -= 1
+        data.writerow(["RH: {:3.1f} % T: {:3.1f} C".format(hum, temp)])
