739738b6dfc455bfc02b3e6cdef1ef7b6ae9c3bc
Ankur Mahesh <mahesh.ankur10@gmail.com>
2019-04-01 17:18:06 -0700

    adding notebook and file to upload to MYSQL database

---
+++ weatherstation/save_to_sql.py
@@ -0,0 +1,54 @@
+import numpy as np
+import datetime as dt
+import os
+import time
+import paramiko
+import pandas as pd
+from paramiko import SSHClient
+from sshtunnel import SSHTunnelForwarder
+import pymysql
+
+
+fname = "data.txt"
+time_interval = 1
+with open(fname, "a") as f:
+    while True:
+        os.system("./read_sensors.py")
+        
+        output = np.loadtxt("current_measurements.txt")
+        temperature1 = output[0]
+        temperature2 = output[1]
+        pressure = output[2]
+        humidity = output[3]
+        
+        current_date = dt.datetime.now()
+        date = current_date.strftime("%H%M%S")
+        
+        data_string = "{} {} {} {} {}".format(date, temperature1, temperature2, pressure, humidity)
+        
+        
+        sql_hostname = 'mysql'
+        sql_username = 'ankurmahesh'
+        sql_password = 'b1C4ZcoJR8lTXMm3EocB46ek'
+        sql_main_database = 'ankurmahesh'
+        sql_port = 3306
+        ssh_host = 'ssh.ocf.berkeley.edu'
+        ssh_user = 'ankurmahesh'
+        ssh_port = 22
+
+        with SSHTunnelForwarder(
+                (ssh_host, ssh_port),
+                ssh_username=ssh_user,
+                ssh_password="2easy4me",
+                remote_bind_address=(sql_hostname, sql_port)) as tunnel:
+            conn = pymysql.connect(host='127.0.0.1', user=sql_username,
+                    passwd=sql_password, db=sql_main_database,
+                    port=tunnel.local_bind_port)
+            #query = '''SELECT VERSION();'''
+            query = "INSERT INTO weatherdata VALUES({},{},{},{},{});".format(date, temperature1, temperature2, pressure, humidity)
+            data = pd.read_sql_query(query, conn)
+            conn.close() 
+
+        time.sleep(time_interval)
+
+
