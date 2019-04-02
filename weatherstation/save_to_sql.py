3bf13bcbf6450369115a34866e31e8c6e2d85f03
Ankur Mahesh <mahesh.ankur10@gmail.com>
2019-04-01 17:27:30 -0700

    save_to_sql.py handles sending to sql database

--- weatherstation/save_to_sql.py
+++ weatherstation/save_to_sql.py
@@ -11,6 +11,14 @@ import pymysql
 
 fname = "data.txt"
 time_interval = 1
+sql_hostname = 'mysql'
+sql_username = 'ankurmahesh'
+sql_password = 'b1C4ZcoJR8lTXMm3EocB46ek'
+sql_main_database = 'ankurmahesh'
+sql_port = 3306
+ssh_host = 'ssh.ocf.berkeley.edu'
+ssh_user = 'ankurmahesh'
+ssh_port = 22
 with open(fname, "a") as f:
     while True:
         os.system("./read_sensors.py")
@@ -22,20 +30,10 @@ with open(fname, "a") as f:
         humidity = output[3]
         
         current_date = dt.datetime.now()
-        date = current_date.strftime("%H%M%S")
+        date = current_date.strftime("%H%M%S")        
         
-        data_string = "{} {} {} {} {}".format(date, temperature1, temperature2, pressure, humidity)
-        
-        
-        sql_hostname = 'mysql'
-        sql_username = 'ankurmahesh'
-        sql_password = 'b1C4ZcoJR8lTXMm3EocB46ek'
-        sql_main_database = 'ankurmahesh'
-        sql_port = 3306
-        ssh_host = 'ssh.ocf.berkeley.edu'
-        ssh_user = 'ankurmahesh'
-        ssh_port = 22
 
+        #TODO: it might be bad to open ssh connection once every time_interval seconds
         with SSHTunnelForwarder(
                 (ssh_host, ssh_port),
                 ssh_username=ssh_user,
@@ -43,10 +41,15 @@ with open(fname, "a") as f:
                 remote_bind_address=(sql_hostname, sql_port)) as tunnel:
             conn = pymysql.connect(host='127.0.0.1', user=sql_username,
                     passwd=sql_password, db=sql_main_database,
-                    port=tunnel.local_bind_port)
-            #query = '''SELECT VERSION();'''
-            query = "INSERT INTO weatherdata VALUES({},{},{},{},{});".format(date, temperature1, temperature2, pressure, humidity)
+                    port=tunnel.local_bind_port, autocommit=True)
+            #Sample ready query
+            query = "SELECT temperature1 from weatherdata;"
             data = pd.read_sql_query(query, conn)
+            
+            insert_command = "INSERT INTO weatherdata VALUES({},{},{},{},{});".format(date, temperature1, \
+                                        temperature2, pressure, humidity)
+            cur = conn.cursor()
+            cur.execute(insert_command)
             conn.close() 
 
         time.sleep(time_interval)
